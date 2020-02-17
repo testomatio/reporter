a12f6e9bc0d5c6568a69db7bccdb0e03be64e301
Koushik Mohan <koushikmohan1996@gmail.com>
2020-02-17 20:50:55 +0530

    Add constants and review changes

--- example/core/index.js
+++ example/core/index.js
@@ -1,12 +1,12 @@
-const { TestomatClient } = require('testomat-reporter');
+const { TestomatClient, TRConstants } = require('testomat-reporter');
 
 const client = new TestomatClient({ apiKey: 'l5x5d5cd6pc3' });
 
 client.createRun().then(async () => {
-  await client.addTestRun('cc883e99', 'passed');
-  await client.addTestRun('7af2c281', 'passed');
-  await client.addTestRun('6603c29e', 'passed');
-  await client.addTestRun('95956b60', 'failed', 'Test case failed');
+  await client.addTestRun('cc883e99', TRConstants.PASSED);
+  await client.addTestRun('7af2c281', TRConstants.PASSED);
+  await client.addTestRun('6603c29e', TRConstants.PASSED);
+  await client.addTestRun('95956b60', TRConstants.FAILED, 'Test case failed');
 
-  client.updateRunStatus('failed');
-});
+  client.updateRunStatus(TRConstants.FAILED);
+}).catch(console.log);
