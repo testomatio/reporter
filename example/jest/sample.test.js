39f96044b4476bc3e752b5a96ebfa7430a7fff46
Koushik Mohan <koushikmohan1996@gmail.com>
2020-02-27 19:13:54 +0530

    Add support for workers

--- example/jest/sample.test.js
+++ example/jest/sample.test.js
@@ -5,3 +5,7 @@ test('This is a sample test 3', () => {
 test('This is a sample test 4', () => {
   expect(1 + 2).toBe(3);
 });
+
+test('Test for suite 2 @Tc2171bd2', () => {
+  expect(1 + 2).toBe(3);
+});
