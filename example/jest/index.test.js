dffc166de3c2eaa5c40247228ace88fa6f0513c9
Koushik Mohan <koushikmohan1996@gmail.com>
2020-04-30 21:04:34 +0530

    Add error and duration

--- example/jest/index.test.js
+++ example/jest/index.test.js
@@ -1,7 +1,7 @@
-test('Test issue for suite 1 @Tca70c8c4', () => {
+test('Test issue for suite 1', () => {
   expect(1 + 2).toBe(3);
 });
 
-test('Test for suite 2 @Tc2171bd2', () => {
+test('Test for suite 2', () => {
   expect(1 + 2).toBe(3);
 });
--- example/jest/sample.test.js
+++ example/jest/sample.test.js
@@ -6,6 +6,6 @@ test('This is a sample test 4', () => {
   expect(1 + 2).toBe(3);
 });
 
-test('Test for suite 2 @Tc2171bd2', () => {
+test('Test for suite 2', () => {
   expect(1 + 2).toBe(3);
 });
