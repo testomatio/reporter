99b32cc4bef7227a1aee69690dd39f6e96be3ce4
Oleksandr Pelykh <olexandr13@gmail.com>
2023-11-01 19:09:33 +0200

    format code

--- tests-storage/reporter-functions.test.js
+++ tests-storage/reporter-functions.test.js
@@ -12,17 +12,6 @@ describe('Testomat reporter functions', () => {
     fileSystem.clearDir(TESTOMAT_TMP_STORAGE_DIR);
   });
 
-  /* failed when run in this scope
-  it('log using testomat.log function @T00000017', () => {
-    const message = '- - - - - - - - - - - - - - - - - -- - - - - - - - -- - - - - -test log message';
-    testomat.log(message);
-    const logFilePath = path.join(TESTOMAT_TMP_STORAGE_DIR, 'log', 'log_00000017');
-    expect(fs.existsSync(logFilePath)).to.equal(true);
-    const logContent = removeColorCodes(fs.readFileSync(logFilePath, 'utf8'));
-    expect(logContent).to.equal(`${message}`);
-  });
-  */
-
   it('step using testomat.step function @T00000018', () => {
     const message = 'test step message';
     testomat.step(message);
