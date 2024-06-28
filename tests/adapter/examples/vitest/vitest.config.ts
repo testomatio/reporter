e885d6649845cd3b2f92b906ae5de044d86ee63e
Oleksandr Pelykh <olexandr13@gmail.com>
2024-06-28 18:05:05 +0300

    upd example config

--- tests/adapter/examples/vitest/vitest.config.ts
+++ tests/adapter/examples/vitest/vitest.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vitest/config';
-// import { TestomatioReporter } from './reporter.ts'
-// import { VitestReporter } from './node_modules/@testomatio/reporter/lib/adapter/vitest.js';
 import TestomatioReporter from '../../../../lib/adapter/vitest.js';
 
 export default defineConfig({
   test: {
-    // reporters: ['verbose'],
-    // reporters: ['verbose', new VitestReporter()],
-
-    // both work
-    // reporters: ['verbose', './node_modules/@testomatio/reporter/lib/adapter/vitest-common.js'],
     reporters: ['verbose', new TestomatioReporter()],
     watch: false,
   },
