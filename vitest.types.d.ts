3d45ab9feeea23fc4e6a77f5baf1c7b2a1c1e519
Oleksandr Pelykh <olexandr13@gmail.com>
2024-06-28 17:41:45 +0300

    improve vitest types

--- vitest.types.d.ts
+++ vitest.types.d.ts
@@ -1,5 +1,5 @@
-import { TaskBase, TaskState } from 'vitest';
-
+type RunMode = 'run' | 'skip' | 'only' | 'todo';
+type TaskState = RunMode | 'pass' | 'fail';
 type Task = Test | Suite | File;
 
 interface File extends Suite {
@@ -13,6 +13,59 @@ interface Suite extends TaskBase {
   tasks: Task[];
   filepath?: string;
   projectName: string;
+}
+
+type ErrorWithDiff = {
+  cause: string;
+  name: string;
+  nameStr?: string;
+  stack?: string;
+  stackStr?: string;
+  stacks?: {
+    method: string;
+    file: string;
+    line: number;
+    column: number;
+  },
+  showDiff?: boolean;
+  actual?: any;
+  expected?: any;
+  operator?: string;
+  type?: string;
+  frame?: string;
+  diff?: string;
+  codeFrame?: string;
+}
+
+interface TaskResult {
+  state: TaskState;
+  duration?: number;
+  startTime?: number;
+  heap?: number;
+  errors?: ErrorWithDiff[];
+  htmlError?: string;
+  hooks?: Partial<Record<'beforeAll' | 'afterAll' | 'beforeEach' | 'afterEach', TaskState>>;
+  retryCount?: number;
+  repeatCount?: number;
+}
+
+interface TaskBase {
+  id: string;
+  name: string;
+  mode: RunMode;
+  meta: {};
+  each?: boolean;
+  concurrent?: boolean;
+  shuffle?: boolean;
+  suite?: Suite;
+  file?: File;
+  result?: TaskResult;
+  retry?: number;
+  repeats?: number;
+  location?: {
+    line: number;
+    column: number;
+  };
 }
 
 export type TestLogs = {
@@ -31,19 +84,6 @@ type FileResult = {
   },
   startTime: number,
   state: TaskState,
-}
-
-type TestError = {
-  actual: string, // actual value
-  diff: string, // diff between actual and expected values
-  expected: string, // expected value
-  message: string, // error message
-  name: string, // error category name (e.g. AssertionError)
-  nameStr: string, // error category name (e.g. AssertionError)
-  operator: string, // e.g. strictEqual
-  showDiff: boolean,
-  stack: string,
-  stackStr: string,
 }
 
 export type Test = TaskBase & {
