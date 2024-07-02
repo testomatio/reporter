31c62d7005cbd598188554e5dd42ee391aa8ccd6
AZANIR <AZANIR@users.noreply.github.com>
2024-07-02 17:07:06 +0000

    deploy: 4c438380852c2412a2f79249effaa0628455b46c

---
+++ tests/adapter/config/index.js
@@ -0,0 +1,10 @@
+const host = 'localhost';
+const port = 19000;
+
+module.exports = {
+  host,
+  port,
+  TESTOMATIO_URL: `http://${host}:${port}`,
+  TESTOMATIO: 'e84d71b06590', // example api key
+  RUN_ID: '0957aa26', // example run id
+};
---
+++ tests/adapter/examples/jasmine/index.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+describe('Suite 1', () => {
+  it('Test addition', () => {
+    expect(1 + 2).toBe(4);
+  });
+
+  it('Test some more addition', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
