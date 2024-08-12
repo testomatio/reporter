9302f688e1b0bbbf4159bb182e406881b1538e69
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2024-08-12 21:20:22 +0300

    replace chalk with picocolors

--- lib/adapter/mocha/edit-package-json.sh
+++ lib/adapter/mocha/edit-package-json.sh
@@ -8,6 +8,6 @@ file="lib-cjs/package.json"
 
 # Use sed to perform the replacements
 sed -i '' 's/"type": "module"/"type": "commonjs"/' "$file"
-sed -i '' 's/"chalk": "[^"]*"/"chalk": "^4.1.2"/' "$file"
+# sed -i '' 's/"chalk": "[^"]*"/"chalk": "^4.1.2"/' "$file"
 
-echo "Updated 'type' to 'commonjs' and 'chalk' version to '^4.1.2' in $file"
+echo "Updated 'type' to 'commonjs' in $file"
