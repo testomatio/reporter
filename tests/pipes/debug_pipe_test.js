ce459d319b41bd02d80504505d90cb5e761fb56c
AZANIR <eleoneks@gmail.com>
2024-10-18 15:10:12 +0300

    fix tests and names

--- tests/pipes/debug_pipe_test.js
+++ tests/pipes/debug_pipe_test.js
@@ -14,53 +14,41 @@ describe('DebugPipe logging tests', () => {
   beforeEach(() => {
     process.env.TESTOMATIO_DEBUG = 1;
     debugPipe = new DebugPipe({});
-
     logFilePath = debugPipe.logFileName;
-
     expect(fs.existsSync(logFilePath)).to.be.true;
   });
 
-  afterEach(async () => {
+  afterEach(() => {
     if (fs.existsSync(logFilePath)) {
-      await fs.promises.rm(logFilePath, { recursive: true });
+      fs.rmSync(logFilePath, { recursive: true });
     }
   });
 
   it('should create log file and write log data when enabled', async () => {
     await debugPipe.logToFile(LOG_DATA);
-    await new Promise(resolve => {
-      setTimeout(resolve, 500);
-    });
     const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
     expect(savedData.length).to.equal(3);
-
     expect(savedData[0]).to.equal(
       JSON.stringify({
-        actions: 'preconditions',
+        data: 'variables',
         testomatioEnvVars: debugPipe.testomatioEnvVars,
       }),
     );
-
-    expect(savedData[1]).to.equal(JSON.stringify({ actions: 'store', store: {} }));
+    expect(savedData[1]).to.equal(JSON.stringify({ data: 'store', store: {} }));
     expect(savedData[2]).to.equal(JSON.stringify(LOG_DATA));
   });
 
   it('should not log data when TESTOMATIO_DEBUG is not set', async () => {
-    debugPipe.isEnabled = false;
-    await debugPipe.logToFile(LOG_DATA);
-
-    const savedData = await fs.readFileSync(debugPipe.logFileName, 'utf-8');
-    expect(savedData).to.equal('');
+    delete process.env.TESTOMATIO_DEBUG;
+    const debugPipeWithoutLogging = new DebugPipe({});
+    await debugPipeWithoutLogging.logToFile(LOG_DATA);
+    expect(fs.existsSync(debugPipeWithoutLogging.logFileName)).to.be.false;
   });
 
   it('should handle batch upload and log multiple tests in a batch', async () => {
     debugPipe.batch.tests = [{ id: 'test1' }, { id: 'test2' }];
     await debugPipe.batchUpload();
-    await new Promise(resolve => {
-      setTimeout(resolve, 500);
-    });
     const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
-
     expect(savedData.length).to.equal(3);
     expect(savedData[2]).to.equal(
       JSON.stringify({ action: 'addTestsBatch', tests: [{ id: 'test1' }, { id: 'test2' }] }),
@@ -70,13 +58,8 @@ describe('DebugPipe logging tests', () => {
   it('should clear interval on finishRun and save final log', async () => {
     debugPipe.isBatchEnabled = true;
     debugPipe.batch.intervalFunction = setInterval(() => {}, 5000);
-
     await debugPipe.finishRun({});
-    await new Promise(resolve => {
-      setTimeout(resolve, 500);
-    });
     const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
-
     expect(savedData.some(line => line.includes('"actions":"finishRun"'))).to.be.true;
   });
 
@@ -84,7 +67,6 @@ describe('DebugPipe logging tests', () => {
     await debugPipe.logToFile({ action: 'firstTest' });
     await debugPipe.logToFile({ action: 'secondTest' });
     const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
-
     expect(savedData.length).to.equal(4);
     expect(savedData[2]).to.equal(JSON.stringify({ action: 'firstTest' }));
     expect(savedData[3]).to.equal(JSON.stringify({ action: 'secondTest' }));
