ce16bf3edd0b7bbf428d1186a09337882646f151
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2024-10-21 19:36:00 +0300

    upd tests

--- tests/pipes/debug_pipe_test.js
+++ tests/pipes/debug_pipe_test.js
@@ -26,16 +26,18 @@ describe('DebugPipe logging tests', () => {
 
   it('should create log file and write log data when enabled', async () => {
     await debugPipe.logToFile(LOG_DATA);
-    const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
-    expect(savedData.length).to.equal(3);
-    expect(savedData[0]).to.equal(
+    const savedData = fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
+    expect(savedData.length).to.equal(4);
+    expect(savedData[1]).to.contain(
       JSON.stringify({
         data: 'variables',
         testomatioEnvVars: debugPipe.testomatioEnvVars,
-      }),
+      })
+      // cut curly braces from the beginning and the end
+      .slice(1, -1),
     );
-    expect(savedData[1]).to.equal(JSON.stringify({ data: 'store', store: {} }));
-    expect(savedData[2]).to.equal(JSON.stringify(LOG_DATA));
+    expect(savedData[2]).to.contain(JSON.stringify({ data: 'store', store: {} }).slice(1, -1));
+    expect(savedData[3]).to.contain(JSON.stringify(LOG_DATA).slice(1, -1));
   });
 
   it('should not log data when TESTOMATIO_DEBUG is not set', async () => {
@@ -48,10 +50,10 @@ describe('DebugPipe logging tests', () => {
   it('should handle batch upload and log multiple tests in a batch', async () => {
     debugPipe.batch.tests = [{ id: 'test1' }, { id: 'test2' }];
     await debugPipe.batchUpload();
-    const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
-    expect(savedData.length).to.equal(3);
-    expect(savedData[2]).to.equal(
-      JSON.stringify({ action: 'addTestsBatch', tests: [{ id: 'test1' }, { id: 'test2' }] }),
+    const savedData = fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
+    expect(savedData.length).to.equal(4);
+    expect(savedData[3]).to.contain(
+      JSON.stringify({ action: 'addTestsBatch', tests: [{ id: 'test1' }, { id: 'test2' }] }).slice(1, -1),
     );
   });
 
@@ -59,16 +61,16 @@ describe('DebugPipe logging tests', () => {
     debugPipe.isBatchEnabled = true;
     debugPipe.batch.intervalFunction = setInterval(() => {}, 5000);
     await debugPipe.finishRun({});
-    const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
+    const savedData = fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
     expect(savedData.some(line => line.includes('"actions":"finishRun"'))).to.be.true;
   });
 
   it('should append data to the same log file', async () => {
     await debugPipe.logToFile({ action: 'firstTest' });
     await debugPipe.logToFile({ action: 'secondTest' });
-    const savedData = await fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
-    expect(savedData.length).to.equal(4);
-    expect(savedData[2]).to.equal(JSON.stringify({ action: 'firstTest' }));
-    expect(savedData[3]).to.equal(JSON.stringify({ action: 'secondTest' }));
+    const savedData = fs.readFileSync(logFilePath, 'utf-8').trim().split('\n');
+    expect(savedData.length).to.equal(5);
+    expect(savedData[3]).to.contain(JSON.stringify({ action: 'firstTest' }).slice(1, -1));
+    expect(savedData[4]).to.contain(JSON.stringify({ action: 'secondTest' }).slice(1, -1));
   });
 });
