a871b07d2f4aba9e332a02ce380571012c7ac86a
AZANIR <AZANIR@users.noreply.github.com>
2024-12-20 23:03:21 +0000

    deploy: 0386322a7432bb9bd7631f3938e967657963f4fa

---
+++ tests/adapter/examples/jest/index.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+describe('Suite 1', () => {
+  test('Test addition', () => {
+    expect(1 + 2).toBe(4);
+  });
+
+  test('Test some more addition', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
