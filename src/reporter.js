852e1e1ecb87202c6a2f296cfb2eae39a2671d00
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2025-01-15 19:09:09 +0200

    add types for reporter

--- src/reporter.js
+++ src/reporter.js
@@ -3,6 +3,20 @@
 import { services } from './services/index.js';
 import reporterFunctions from './reporter-functions.js';
 
+
+export const artifact = reporterFunctions.artifact;
+export const log = reporterFunctions.log;
+export const logger = services.logger;
+export const meta = reporterFunctions.keyValue;
+export const step = reporterFunctions.step;
+
+/**
+ * @typedef {import('./reporter-functions.js')} artifact
+ * @typedef {import('./reporter-functions.js')} log
+ * @typedef {import('./services/index.js')} logger
+ * @typedef {import('./reporter-functions.js')} meta
+ * @typedef {import('./reporter-functions.js')} step
+ */
 export default {
   /**
    * @deprecated Use `log` or `testomat.log`
@@ -18,9 +32,3 @@ export default {
   // TestomatClient,
   // TRConstants,
 };
-
-export const artifact = reporterFunctions.artifact;
-export const log = reporterFunctions.log;
-export const logger = services.logger;
-export const meta = reporterFunctions.keyValue;
-export const step = reporterFunctions.step;
