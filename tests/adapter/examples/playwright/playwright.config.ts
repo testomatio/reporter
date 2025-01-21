5beecc93aea16bf73278e6ee785dd8ab122f6f75
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2025-01-21 18:06:31 +0200

    upd pw type import

--- tests/adapter/examples/playwright/playwright.config.ts
+++ tests/adapter/examples/playwright/playwright.config.ts
@@ -1,5 +1,7 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
+// test fails on CI with this type if NODE=v23:
+// SyntaxError: The requested module '@playwright/test' does not provide an export named 'PlaywrightTestConfig'
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   testMatch: '/tests/*.spec.js',
