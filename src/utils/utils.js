8a6a82aa142c6d67698c7418088c096956875757
DavertMik <davert@testomat.io>
2025-05-23 01:43:46 +0300

    added fix for xml with ids

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,7 +10,9 @@ import { fileURLToPath } from 'url';
 const debug = createDebugMessages('@testomatio/reporter:util');
 
 // Use __dirname directly since we're compiling to CommonJS
-const __dirname = path.resolve();
+// @ts-ignore - import.meta is only available in ESM
+const currentDir = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = typeof global.__dirname !== 'undefined' ? global.__dirname : currentDir;
 
 /**
  * @param {String} testTitle - Test title
