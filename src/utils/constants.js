467ae72b46869734e482879d27829b1caebbcdbe
AZANIR <eleoneks@gmail.com>
2025-07-03 13:36:00 +0300

    Refactor constants and improve test readability
    - Changed the export method of the extensionMap in constants.js to ES6 module syntax for better compatibility.
    - Cleaned up whitespace in codecept.test.js and playwright.test.js to enhance readability and maintain consistency.
    - Ensured consistent formatting in test descriptions and assertions across both test files.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,12 @@
-"use strict";
-
-const extensionMap = {
-    'application/json': 'json',
-    'text/plain': 'txt',
-    'image/jpeg': 'jpg',
-    'image/png': 'png',
-    'text/html': 'html',
-    'text/css': 'css',
-    'text/javascript': 'js',
-    'application/pdf': 'pdf',
-    'application/xml': 'xml',
-    'text/xml': 'xml'
+export const extensionMap = {
+  'application/json': 'json',
+  'text/plain': 'txt',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'text/html': 'html',
+  'text/css': 'css',
+  'text/javascript': 'js',
+  'application/pdf': 'pdf',
+  'application/xml': 'xml',
+  'text/xml': 'xml',
 };
-
-module.exports = {
-    extensionMap
-}; 
