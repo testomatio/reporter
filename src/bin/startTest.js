8685abacaf11da9c76560601154b870e48422928
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2025-10-06 21:00:04 +0300

    add old cli deprecation warning

--- src/bin/startTest.js
+++ src/bin/startTest.js
@@ -11,6 +11,16 @@ const cliPath = join(__dirname, 'cli.js');
 const version = getPackageVersion();
 console.log(pc.cyan(pc.bold(` 🤩 Testomat.io Reporter v${version}`)));
 
+// Deprecation warning
+console.log('⚠️', pc.yellow('The "start-test-run" command is deprecated.'));
+console.log(
+  pc.yellow('Please use'),
+  pc.cyan(pc.bold('"npx @testomatio/reporter run"')),
+  'or',
+  pc.cyan(pc.bold('"npx @testomatio/reporter test"')),
+  'instead.',
+);
+
 // Parse command line arguments to map start-test-run options to @testomatio/reporter run format
 const args = process.argv.slice(2);
 const newArgs = ['run'];
@@ -18,7 +28,7 @@ const newArgs = ['run'];
 let i = 0;
 while (i < args.length) {
   const arg = args[i];
-  
+
   if (arg === '-c' || arg === '--command') {
     // Map -c/--command to positional argument for run command
     i++;
@@ -33,7 +43,7 @@ while (i < args.length) {
     // Map --launch to start command
     newArgs[0] = 'start';
   } else if (arg === '--finish') {
-    // Map --finish to finish command  
+    // Map --finish to finish command
     newArgs[0] = 'finish';
   } else {
     // Pass through other arguments
@@ -45,9 +55,9 @@ while (i < args.length) {
 // Execute the main CLI with mapped arguments
 
 const child = spawn(process.execPath, [cliPath, ...newArgs], {
-  stdio: 'inherit'
+  stdio: 'inherit',
 });
 
-child.on('exit', (code) => {
+child.on('exit', code => {
   process.exit(code);
 });
