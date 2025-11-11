08eb1ab81609077d4b02af7d66a729588a17385e
DavertMik <davert@testomat.io>
2025-11-11 17:00:24 +0200

    fixed minor issues

--- src/xmlReader.js
+++ src/xmlReader.js
@@ -274,7 +274,9 @@ class XmlReader {
   _parseTRXTestDefinition(td) {
     const title = td.name.replace(/\(.*?\)/, '').trim();
     const exampleMatch = td.name.match(/\((.*?)\)/);
-    const example = exampleMatch ? { ...exampleMatch[1].split(',') } : null;
+    const example = exampleMatch ? {
+      ...exampleMatch[1].split(',').map(p => p.trim()).filter(p => p !== '')
+    } : null;
 
     const suite = td.TestMethod.className.split(', ')[0].split('.');
     const suite_title = suite.pop();
@@ -598,7 +600,7 @@ function reduceTestCases(prev, item) {
 
       const exampleMatches = testCaseItem.name?.match(/\S\((.*?)\)/);
       if (exampleMatches) {
-        example = { ...exampleMatches[1].split(',').map(v => v.trim().replace(/[^\w\s-]/g, '')) };
+        example = { ...exampleMatches[1].split(',').map(v => v.trim().replace(/[^\w\s-]/g, '')).filter(v => v !== '') };
         title = title.replace(/\(.*?\)/, '').trim();
       }
 
