910160cd65c7ebce0668aa1144c7cb82b2fd646d
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2025-11-11 18:42:05 +0200

    fix unit test

--- tests/unit/log_formatter_test.js
+++ tests/unit/log_formatter_test.js
@@ -1,36 +1,35 @@
 import { expect } from 'chai';
-import TestomatioClient from '../../src/client.js';
 import errorFn from './data/src/error.js';
+import { formatError } from '../../src/utils/log-formatter.js';
+
 const ANSI_REGEX = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
 
-describe('Client', () => {
+describe('log formatter formatError()', () => {
   afterEach(() => {
     process.env.TESTOMATIO_STACK_IGNORE = null;
   });
 
   it('should include filter for stack trace', () => {
-    const client = new TestomatioClient();
-
     let error = new Error('Test error');
-    let stack = client.formatError(error);
-    expect(stack).to.include('client_test.js');
+    let stack = formatError(error);
+    expect(stack).to.include('log_formatter_test.js');
 
-    process.env.TESTOMATIO_STACK_IGNORE = '**/client_test.js';
+    process.env.TESTOMATIO_STACK_IGNORE = '**/log_formatter_test.js';
 
     error = new Error('Test error');
-    stack = client.formatError(error);
-    expect(stack).not.to.include('client_test.js');
+    stack = formatError(error);
+    expect(stack).not.to.include('log_formatter_test.js');
 
     try {
       process.env.TESTOMATIO_STACK_IGNORE = null;
       errorFn();
       expect.fail('Should throw error');
     } catch (e) {
-      stack = client.formatError(e);
+      stack = formatError(e);
       // prettier-ignore
       expect(stack.replace(ANSI_REGEX, '')).to.include('throw new Error(\'Test error\')');
       expect(stack).to.include('data/src/error.js');
-      expect(stack).to.include('client_test.js');
+      expect(stack).to.include('log_formatter_test.js');
     }
 
     try {
@@ -38,12 +37,12 @@ describe('Client', () => {
       errorFn();
       expect.fail('Should throw error');
     } catch (e) {
-      stack = client.formatError(e);
+      stack = formatError(e);
 
       // prettier-ignore
       expect(stack).not.to.include('throw new Error(\'Test error\')');
       expect(stack).not.to.include('data/src/error.js');
-      expect(stack).to.include('client_test.js');
+      expect(stack).to.include('log_formatter_test.js');
     }
   });
 });
