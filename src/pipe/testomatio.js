00bdbb90836315155bc3c0396f80caada53abf8f
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2026-02-02 19:36:59 +0200

    properly replace api key in logs

--- src/pipe/testomatio.js
+++ src/pipe/testomatio.js
@@ -583,7 +583,7 @@ function printCreateIssue() {
  * @returns {string}
  */
 function hideTestomatioToken(data) {
-  return data.replace(/"api_key": "[^"]+"/g, '"api_key": "<hidden>"').replace(/"(tstmt_[^"]+)"/g, 'tstmt_***');
+  return data.replace(/"api_key": "[^"]+"/g, '"api_key": "<hidden>"').replace(/"(tstmt_[^"]+)"/g, '"tstmt_***"');
 }
 
 /**
