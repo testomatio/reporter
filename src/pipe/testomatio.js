1a46a929d734a2ce6c41316b1d0abfd515835af8
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2026-06-08 23:17:06 +0300

    revert type

--- src/pipe/testomatio.js
+++ src/pipe/testomatio.js
@@ -46,6 +46,7 @@ function parseCiParams(raw) {
 /**
  * @typedef {import('../../types/types.js').Pipe} Pipe
  * @typedef {import('../../types/types.js').TestData} TestData
+ * @typedef {import('../../types/types.js').BatchMode} BatchMode
  * @typedef {import('../../types/types.js').CreateRunParams} CreateRunParams
  * @class TestomatioPipe
  * @implements {Pipe}
@@ -53,7 +54,8 @@ function parseCiParams(raw) {
 class TestomatioPipe {
   constructor(params, store) {
     this.batch = {
-      /** Batch upload mode:
+      /** @type {BatchMode}
+       * Batch upload mode:
        * - `auto`: upload tests automatically by time interval (e.g. every 5 seconds).
        * - `manual`: buffer tests and upload only when `sync()` is invoked manually.
        * - `disabled`: send one test per request, no batching.
